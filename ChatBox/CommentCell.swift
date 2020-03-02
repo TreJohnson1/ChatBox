bf469814ed1d0523800c09456857f3d4ebeb02f8
Tre Johnson <tzjohnson18@students.desu.edu@Tres-MacBook-Pro.local>
2020-03-01 19:09:03 -0500

    secnd commit

---
+++ ChatBox/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  ChatBox
+//
+//  Created by Tre Johnson on 3/1/20.
+//  Copyright © 2020 Tre Johnson. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
